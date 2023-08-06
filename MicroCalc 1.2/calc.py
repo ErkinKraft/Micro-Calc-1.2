95415da84697b4b735ea5dc803c0764f7aa7c76e
ErkinKraft <96366009+ErkinKraft@users.noreply.github.com>
2023-08-06 22:37:41 +0300

    Update calc.py

--- MicroCalc 1.2/calc.py
+++ MicroCalc 1.2/calc.py
@@ -1,6 +1,8 @@
 while True:
     nam1 =int(input("Введите первое число",))
     nam2 =int(input("Введите второе число",))
+    nam1 =float(nam1)
+    nam2 =float(nam2)
     print("Результат +:", nam1+nam2)
     print("Результат -:", nam1-nam2)
     print("Результат *:", nam1*nam2)
